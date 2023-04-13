0a6cbd555d1bc86f409aa56e10b7c67e2752bf21
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-04-13 18:50:21 +0100

    1-1 / Added barriers to the pits to reset the level when player falls in or destroy game objects if enemy falls in

---
+++ Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,18 @@
+using Assets.Scripts.Helpers;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Constants.TAG_PLAYER))
+        {
+            collision.gameObject.SetActive(false);
+            GameManager.Instance.ResetLevel(3f);
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
